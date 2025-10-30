3c79a97119a688cde422cc7bc09519a440ac2486
GwonMinji <75905963+choie1346@users.noreply.github.com>
2025-10-30 17:40:50 +0900

    state_machine 가져옴

--- Animal.py
+++ Animal.py
@@ -1,4 +1,6 @@
 from pico2d import load_image
+from state_machine import StateMachine
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE, SDLK_RIGHT, SDL_KEYUP, SDLK_LEFT, SDLK_n
 
 # 0-front, 1-back, 2-left, 3-right
 # route1 - 3까지, route2 - 4부터 8까지
@@ -45,16 +47,49 @@ image_dirs = [
     ]
 ]
 
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+def key_n(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_n
+
+def right_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_RIGHT
+def right_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_RIGHT
+def left_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_LEFT
+def left_up(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYUP and e[1].key == SDLK_LEFT
+
+class front:
+    def __init__(self, animal):
+        self.animal = animal
+
+    def enter(self, e):
+        self.animal.image = load_image(image_dirs[0][self.animal.current])
+
+    def do(self):
+        self.animal.frame = (self.animal.frame + 1) % 4
+
+    def exit(self, e):
+        pass
+
 class Animal:
     def __init__(self):
         self.x, self.y = 400, 350
         self.route = 1
         self.current = 0
         self.frame = 0
-        self.image = load_image('Animals/Chicken/front.png') # Chicken
-
+        #self.image = load_image('Animals/Chicken/front.png') # Chicken
+        self.FRONT = front(self)
+        self.state_machine = StateMachine(
+            self.FRONT,  # initial state
+            {
+                self.FRONT: {}
+            }
+        )
     def update(self):
-        self.frame = (self.frame + 1) % 4
+        self.state_machine.update()
         pass
 
     def draw(self):
